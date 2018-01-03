339cab44bad43cc76d38c2fa3804a27dcc901f1a
Franco Valencia <franco.avalencia@gmail.com>
2018-01-02 19:58:26 -0600

    Bumped version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='krakenpy',
-    version='0.1.0',
+    version='0.1.1',
     description='Unofficial Python3 library for the Kraken API',
     long_description=readme,
     author='Franco Valencia',
